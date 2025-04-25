f051e234b3c7048f36e163bb042525f51b06afce
ayman <danishaiman3b@gmail.com>
2025-04-25 11:17:20 +0800

    Add Employee ID field to user registration and update related files

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -184,7 +184,7 @@ namespace CLIP.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email, EmpID = model.EmpID };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
