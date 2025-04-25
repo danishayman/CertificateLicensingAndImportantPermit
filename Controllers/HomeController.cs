09b729d6c1dddfa4f837e4f98068608b84e712f5
ayman <danishaiman3b@gmail.com>
2025-04-25 14:29:49 +0800

    Implement delete Competency

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CLIP.Models;
 
 namespace CLIP.Controllers
 {
@@ -38,6 +39,67 @@ namespace CLIP.Controllers
                 return RedirectToAction("Index");
             }
             return View();
+        }
+
+        [Authorize]
+        public ActionResult Competency()
+        {
+            // Get list of all competency modules
+            var db = new ApplicationDbContext();
+            var competencyModules = db.CompetencyModules.ToList();
+            
+            return View(competencyModules);
+        }
+
+        [Authorize]
+        public ActionResult AddCompetency()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddCompetency(CompetencyModule model)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = new ApplicationDbContext();
+                db.CompetencyModules.Add(model);
+                db.SaveChanges();
+                
+                return RedirectToAction("Competency");
+            }
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteCompetency(int id)
+        {
+            var db = new ApplicationDbContext();
+            var competency = db.CompetencyModules.Find(id);
+            
+            if (competency != null)
+            {
+                // Check if the competency is in use before deleting
+                bool isInUse = db.UserCompetencies.Any(uc => uc.CompetencyModuleId == id);
+                
+                if (isInUse)
+                {
+                    TempData["ErrorMessage"] = "This competency cannot be deleted because it is assigned to one or more users.";
+                }
+                else
+                {
+                    db.CompetencyModules.Remove(competency);
+                    db.SaveChanges();
+                    TempData["SuccessMessage"] = "Competency module deleted successfully.";
+                }
+            }
+            
+            return RedirectToAction("Competency");
         }
     }
 }
